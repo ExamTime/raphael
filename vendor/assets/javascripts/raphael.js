c4dcd9a0051f123e74942f765fed00114dfa1da4
Domhnall Murphy <domhnall.murphy@examtime.com>
2020-01-28 11:51:34 +0000

    Change criteria for detecting touch support (ExamTime/ExamTime#20460)

--- vendor/assets/javascripts/raphael.js
+++ vendor/assets/javascripts/raphael.js
@@ -465,6 +465,25 @@
             }
         }
     }
+    R.is_touch_enabled = function(){
+      var result = false;
+      if (window.PointerEvent && ('maxTouchPoints' in navigator)) {
+        // if Pointer Events are supported, just check maxTouchPoints
+        if (navigator.maxTouchPoints > 0) {
+          result = true;
+        }
+      } else {
+        // no Pointer Events...
+        if (window.matchMedia && window.matchMedia("(any-pointer:coarse)").matches) {
+          // check for any-pointer:coarse which mostly means touchscreen
+          result = true;
+        } else if (window.TouchEvent || ('ontouchstart' in window)) {
+          // last resort - check for exposed touch events API / event handler
+          result = true;
+        }
+      }
+      return result;
+    };
     R.version = "2.1.0";
     R.eve = eve;
     var loaded,
@@ -520,7 +539,7 @@
         appendChild = "appendChild",
         apply = "apply",
         concat = "concat",
-        supportsTouch = ('ontouchstart' in g.win) || g.win.DocumentTouch && g.doc instanceof DocumentTouch, //taken from Modernizr touch test
+        supportsTouch = R.is_touch_enabled(),
         E = "",
         S = " ",
         Str = String,
